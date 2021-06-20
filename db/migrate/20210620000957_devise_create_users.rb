4b4f9e7a3edaedf125fc69f356c7975200193061
kenji_kimura <bboy.kenji.flex@gmail.com>
2021-06-20 10:52:03 +0900

    deviseの導入

--- db/migrate/20210620000957_devise_create_users.rb
+++ db/migrate/20210620000957_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string  :full_name,         null: false
+      t.string  :dancer_name,       null: false
 
       ## Recoverable
       t.string   :reset_password_token
