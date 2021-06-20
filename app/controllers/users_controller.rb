e9788c577739c44364c1770eb291a80b29784861
kenji_kimura <bboy.kenji.flex@gmail.com>
2021-06-20 11:17:49 +0900

    新規登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,13 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :dancer_name, :email,)
+  end
   
 end
